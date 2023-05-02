fb7f9cc63cd67f5fd8d870d74e74a92b275b0dae
Brittany Brooks <Brit@Jesses-MacBook-Pro.local>
2023-05-01 21:21:34 -0700

    need to finish

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,19 +1,25 @@
 # Import the dependencies.
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
+Base=automap_base
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
