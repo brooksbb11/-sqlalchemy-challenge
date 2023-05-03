1e8fda0dab9b41bbcb6f0ee3332059586cb97b58
Brittany Brooks <Brit@Jesses-MacBook-Pro.local>
2023-05-03 00:32:40 -0700

    add

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -10,10 +10,10 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
+engine=create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
-Base=automap_base
-
+Base=automap_base()
 # reflect the tables
 Base.prepare(autoload_with=engine)
 
@@ -22,15 +22,35 @@ measurement = Base.classes.measurement
 station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session=Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app=Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+
+def welcome():
+    """List all available api routes"""
+
+    return (
+        f"Avaiable Routes for HI Weather:<br/>"
+        f"-- Daily Precipitation for 2016-2017 Season: <a 
+        href=\"api/v1.0/precipitation\">/api/v1.0/precipitation<a><br/>"
+        f"-- Weather Stations: <a
+        href=\"api/v1.0/stations\">/api/v1.0/stations<a><br/>"
+        f"-- Daily Temperature Observations for Station USC00519281 for 
+         2016-2017 Season:<a href=\"api/v1.0/tobs\">/api/v1.0/tobs<a><br/> "
+        f"-- Enter a start date to get the minimum, maximum, and average
+        temperatures for all dates after the specified date:
+        /api/v1.0/yyyy-mm-dd<br>"
+        f"-- Enter both a start and end date to get the minimum, maximum,
+        and average temperatures for that date range:
+        /api/v1.0/yyyy-mm-dd<br>"
+    )
+
+   
